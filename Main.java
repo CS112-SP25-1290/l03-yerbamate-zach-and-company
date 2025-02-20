3b5a9e003d10a4bdbc29119815beb78688ddf03f
Zachcroteau <zachcroteau@gmail.com>
2025-02-20 20:49:01 +0000

    New Instance carried over code from accidental local save

--- Main.java
+++ Main.java
@@ -3,23 +3,41 @@ import java.util.Scanner;
 public class Main
 {
 	public static void main(String[] args)
-	{
+  	{
 		CaffeinatedBeverage[] inventory = new CaffeinatedBeverage[10];
 		Scanner keyboard = new Scanner(System.in);
 		// Prompt user with options in a loop
 		int choice, ounces, brewTemp, count = 0;
 		String name;
 		double price;
-
-		do {
+		
+		do
+    	{
 			System.out.println("1) Enter new Tea");
 			System.out.println("2) Enter new Yerba Mate");
 			System.out.println("3) Exit");
 			System.out.print(">>");
 			choice = keyboard.nextInt();
+			
+			switch (choice)
+			{
+			    case 1: // Tea
+			        System.out.print("Enter name      : ");
+			        name = keyboard.nextLine();
+			        System.out.print("Enter ounces    : ");
+			        ounces = keyboard.nextInt();
+			        System.out.print("Enter price     $ ");
+			        price = keyboard.nextDouble();
+			        System.out.print("Enter brew temperature (in Celsius): ");
+			        brewTemp = keyboard.nextInt();
+			
+			        // Create a Tea, put into array
+			        inventory[count] = new Tea(name, ounces, price, brewTemp);
 
-			switch (choice) {
-				case 1: // Tea
+					System.out.println("Your tea order has been added: " + inventory[count]);
+			        count++;
+					break;
+				case 2:
 					System.out.print("Enter name      : ");
 					name = keyboard.nextLine();
 					System.out.print("Enter ounces    : ");
@@ -28,29 +46,33 @@ public class Main
 					price = keyboard.nextDouble();
 					System.out.print("Enter brew temperature (in Celsius): ");
 					brewTemp = keyboard.nextInt();
-
-					// Create a Tea, put into array
-					inventory[count] = new Tea(name, ounces, price, brewTemp);
-
-					System.out.println("Your tea order has been added: " + inventory[count]);
+					inventory[count] = new YerbaMate(name, ounces, price, brewTemp, 0);
+					System.out.println("Your Yerba Mate order has been added: " + inventory[count]);
 					count++;
 					break;
-
+				
+			
 			}
 
 		} while (choice != 3);
-		findAveragePrice(inventory, inventory.length);
-	}
-
-	public static double findAveragePrice(CaffeinatedBeverage[] inventory, int count) {
-		double total = 0;
-		for (int i = 0; i < count; i++) {
-			// double accu = 0;
-			// accu = accu + inventory[i].getPrice();
-			// accu = (accu / inventory.length);
-			// return accu;
-			total += inventory[i].getPrice();
+		
+		if (choice == 3) {
+			for (int i = 0; i <= inventory.length; i++) {
+				System.out.println(inventory.toString());
+			}
+			System.out.println(findHighestPricedYerbaMate(inventory));
 		}
-		return total / count;
 	}
+}
+
+public static YerbaMate findHighestPricedYerbaMate(CaffeinatedBeverage[] inventory) {
+	double max;
+	Object highestPricedMate = new Object();
+	for (int i = 0; i <= inventory.length; i++) {
+		if (inventory[i] instanceof YerbaMate && inventory[i].getPrice() > max) {
+			max = inventory[i].getPrice();
+			highestPricedMate = inventory[i];
+		}
+	}
+	
 }
