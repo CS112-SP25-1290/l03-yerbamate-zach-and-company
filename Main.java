84b37d5aab9c9581f1d4e527b43799d5f0d0a298
Zachcroteau <zachcroteau@gmail.com>
2025-03-05 18:54:23 +0000

    Finished up the lab. Sorry for the lateness. We had issues getting things synced. Today I was able to get everyone's code consolitdated. It's working as intended now. The findAveragePrice() was implemented in this version. Please don't hesitate to message me about anything pertaining to my/groupmate labs!.

--- Main.java
+++ Main.java
@@ -1,43 +1,41 @@
 import java.util.Scanner;
 
-public class Main
-{
-	public static void main(String[] args)
-  	{
+public class Main {
+	public static void main(String[] args) {
 		CaffeinatedBeverage[] inventory = new CaffeinatedBeverage[10];
 		Scanner keyboard = new Scanner(System.in);
 		// Prompt user with options in a loop
 		int choice, ounces, brewTemp, count = 0;
 		String name;
 		double price;
-		
-		do
-    	{
+
+		do {
 			System.out.println("1) Enter new Tea");
 			System.out.println("2) Enter new Yerba Mate");
 			System.out.println("3) Exit");
 			System.out.print(">>");
 			choice = keyboard.nextInt();
-			
-			switch (choice)
-			{
-			    case 1: // Tea
-			        System.out.print("Enter name      : ");
-			        name = keyboard.nextLine();
-			        System.out.print("Enter ounces    : ");
-			        ounces = keyboard.nextInt();
-			        System.out.print("Enter price     $ ");
-			        price = keyboard.nextDouble();
-			        System.out.print("Enter brew temperature (in Celsius): ");
-			        brewTemp = keyboard.nextInt();
-			
-			        // Create a Tea, put into array
-			        inventory[count] = new Tea(name, ounces, price, brewTemp);
+
+			switch (choice) {
+				case 1: // Tea
+					keyboard.nextLine();
+					System.out.print("Enter name      : ");
+					name = keyboard.nextLine();
+					System.out.print("Enter ounces    : ");
+					ounces = keyboard.nextInt();
+					System.out.print("Enter price     $ ");
+					price = keyboard.nextDouble();
+					System.out.print("Enter brew temperature (in Celsius): ");
+					brewTemp = keyboard.nextInt();
+
+					// Create a Tea, put into array
+					inventory[count] = new Tea(name, ounces, price, brewTemp);
 
 					System.out.println("Your tea order has been added: " + inventory[count]);
-			        count++;
+					count++;
 					break;
 				case 2:
+					keyboard.nextLine();
 					System.out.print("Enter name      : ");
 					name = keyboard.nextLine();
 					System.out.print("Enter ounces    : ");
@@ -50,29 +48,46 @@ public class Main
 					System.out.println("Your Yerba Mate order has been added: " + inventory[count]);
 					count++;
 					break;
-				
-			
+
 			}
 
 		} while (choice != 3);
-		
-		if (choice == 3) {
-			for (int i = 0; i <= inventory.length; i++) {
-				System.out.println(inventory.toString());
-			}
-			System.out.println(findHighestPricedYerbaMate(inventory));
-		}
-	}
-}
 
-public static YerbaMate findHighestPricedYerbaMate(CaffeinatedBeverage[] inventory) {
-	double max;
-	Object highestPricedMate = new Object();
-	for (int i = 0; i <= inventory.length; i++) {
-		if (inventory[i] instanceof YerbaMate && inventory[i].getPrice() > max) {
-			max = inventory[i].getPrice();
-			highestPricedMate = inventory[i];
-		}
-	}
+		if (choice == 3) {
+			for (int i = 0; i < count; i++) {
+				if (inventory[i] != null) {
+					System.out.println(inventory[i]);
+				}
+			}
+			double averagePrice = findAveragePrice(inventory);
+			//Convert double to string with 2 decimal places
+			String sValue = (String) String.format("%.2f", averagePrice);
 	
+			System.out.println("Your average inventory price is : $" + sValue);
+			System.out.println("Your most expensive Yerba Mate : " + findHighestPricedYerbaMate(inventory).getName());
+			
+		}
+}
+public static YerbaMate findHighestPricedYerbaMate(CaffeinatedBeverage[] inventory) {
+	double max = 0;
+	YerbaMate highestPricedMate = null;
+	for (CaffeinatedBeverage beverage : inventory) {
+		if (beverage instanceof YerbaMate && beverage.getPrice() > max) {
+			max = beverage.getPrice();
+			highestPricedMate = (YerbaMate) beverage;
+		} 	
+	}
+		return highestPricedMate;
+}
+	public static double findAveragePrice(CaffeinatedBeverage[] inventory) {
+		double total = 0.0;
+		int count = 0;
+		for (CaffeinatedBeverage beverage : inventory) {
+			if (beverage != null) {
+				total += beverage.getPrice();
+				count++;
+			}
+		}
+		return total / count;
+	}
 }
